f14a32be4a69b96b5a5287dc8e618171bb099e1c
Shahid Ismail <shahid_the_king@hotmail.com>
2020-10-26 17:29:09 +0100

    correcting one last time hopefully

--- run_game.sh
+++ run_game.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 echo "Shahid Ismail"
 mkdir Shahid_Ismail_labb
-cp Guesser.class Shahid_Ismail_labb
 cp Guesser.java Shahid_Ismail_labb
-cp GuessingGame.class Shahid_Ismail_labb
 cp GuessingGame.java Shahid_Ismail_labb
 cd Shahid_Ismail_labb
 echo "Running game from $(pwd)"
